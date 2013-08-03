d2e05f00f1617483a6e05ca6ad1d3396638705e9
Piyush Poddar <catchpiyush@gmail.com>
2013-08-03 20:07:16 +0000

    modified web.js to read file using fs.readFileSync 8

--- web.js
+++ web.js
@@ -4,11 +4,12 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-	var content=fs.readFileSync('index.html','utf-8');
-// response.send(fs.readFileSync(__dirname + '/index.html'));
-//	var fs=require('fs');
 //	var content=fs.readFileSync('index.html','utf-8');
-	response.send(content);
+// 	response.send(fs.readFileSync(__dirname + '/index.html'));
+	var content=fs.readFileSync('index.html');
+	var buf=new Buffer(content);
+
+	response.send(buf.toString() + "Bingo 01");
 });
 
 var port = process.env.PORT || 5000;
